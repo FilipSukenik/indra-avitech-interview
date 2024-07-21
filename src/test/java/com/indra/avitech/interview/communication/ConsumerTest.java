102ecad408744be25e7ce016e614e3595d0e159d
f.sukenik <f.sukenik@sportradar.com>
2024-07-21 13:13:54 +0200

    test: cover SQLException being thrown by command execution ✅

--- src/test/java/com/indra/avitech/interview/communication/ConsumerTest.java
+++ src/test/java/com/indra/avitech/interview/communication/ConsumerTest.java
@@ -1,12 +1,17 @@
 package com.indra.avitech.interview.communication;
 
+import com.indra.avitech.interview.communication.command.AddCommand;
 import com.indra.avitech.interview.communication.command.Command;
+import java.sql.SQLException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 
 class ConsumerTest {
@@ -28,5 +33,15 @@ class ConsumerTest {
     consumer.run();
 
     Assertions.assertTrue(Thread.currentThread().isInterrupted());
+  }
+
+  @Test
+  void processThrowsSQLExceptionExpectRuntimeExceptionBeingRethrown() throws SQLException, InterruptedException {
+
+    doReturn(Mockito.mock(AddCommand.class)).when(consumer).consume();
+    doThrow(new SQLException()).when(consumer).process(any());
+    consumer.setRunning(true);
+
+    Assertions.assertThrows(RuntimeException.class, () -> consumer.run());
   }
 }
