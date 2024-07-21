0eae26f487edde685d93a40471fa66f906ae2a41
f.sukenik <f.sukenik@sportradar.com>
2024-07-21 17:01:43 +0200

    test: make producer test package-private 🎨

--- src/test/java/com/indra/avitech/interview/communication/ProducerTest.java
+++ src/test/java/com/indra/avitech/interview/communication/ProducerTest.java
@@ -3,10 +3,10 @@ package com.indra.avitech.interview.communication;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ProducerTest {
+class ProducerTest {
 
   @Test
-  public void nullMessageBusTest() {
+  void nullMessageBusTest() {
 
     Assertions.assertThrows(IllegalArgumentException.class, () -> new Producer(null));
   }
