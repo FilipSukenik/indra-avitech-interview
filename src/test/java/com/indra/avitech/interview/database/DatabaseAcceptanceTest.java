fb98c1de28af0a7c2962cfcfbadf01f3fa645a43
f.sukenik <f.sukenik@sportradar.com>
2024-07-21 12:34:47 +0200

    style: call format and optimize imports on whole project 🎨

--- src/test/java/com/indra/avitech/interview/database/DatabaseAcceptanceTest.java
+++ src/test/java/com/indra/avitech/interview/database/DatabaseAcceptanceTest.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.sql.SQLException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
